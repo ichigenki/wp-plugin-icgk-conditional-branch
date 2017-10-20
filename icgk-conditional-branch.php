e81148986f4685a541e49271ebf957a80e7b2cf7
ichigenki <SHIMBO@shim-iMac.local>
2017-10-21 00:45:47 +0900

    "icgk-conditional-branch"全部をテストする関数"test_conditional_branch($parent_id, $child_id)"を追加

--- icgk-conditional-branch.php
+++ icgk-conditional-branch.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ICGK Conditional Branch
 Plugin URI:
-Description: 階層構造を持つポストタイプの条件分岐関数集：has_child($post_id), has_parent($post_id), is_tree($parent_id), is_child($parent_id), is_parent($child_id)
+Description: 階層構造を持つポストタイプの条件分岐関数集：has_child($post_id), has_parent($post_id), is_tree($parent_id), is_child($parent_id), is_parent($child_id)。"icgk-conditional-branch"全部をテストするには：test_conditional_branch($parent_id, $child_id)。
 Version: 1.0.0
 Author: ICHIGENKI
 Author URI:
@@ -91,3 +91,35 @@ function is_parent($child_id) {
 		return false;
 	}
 }
+
+// "icgk-conditional-branch"をテストする
+function test_conditional_branch($parent_id, $child_id) {
+echo '<p>';
+if( has_child() ) {
+	echo '子ページを持っています。<br />'."\n";
+} else {
+	echo '子ページを持っていません。<br />'."\n";
+}
+if( has_parent() ) {
+	echo '親ページを持っています。<br />'."\n";
+} else {
+	echo '親ページを持っていません。<br />'."\n";
+}
+if( is_tree($parent_id) ) {
+	echo '「'.get_the_title($parent_id).'」または「'.get_the_title($parent_id).'」の子孫ページです。<br />'."\n";
+} else {
+	echo '「'.get_the_title($parent_id).'」とは関係のないページです。<br />'."\n";
+}
+if( is_child($parent_id) ) {
+	echo '「'.get_the_title($parent_id).'」の直接の子ページです。<br />'."\n";
+} else {
+	echo '「'.get_the_title($parent_id).'」の直接の子ページではありません。<br />'."\n";
+}
+if( is_parent($child_id) ) {
+	echo '「'.get_the_title($child_id).'」の直接の親ページです。<br />'."\n";
+} else {
+	echo '「'.get_the_title($child_id).'」の直接の親ページではありません。<br />'."\n";
+}
+echo '</p>'."\n";
+}
+
